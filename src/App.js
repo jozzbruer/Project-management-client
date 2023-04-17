5cef5f72fd5513e7fcf0efac3e77cd1cb9368d83
Joz-Bruer Quince <quincejozbruer@gmail.com>
2023-04-16 22:26:35 -0700

    test the first request

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import './App.css';
+import Clients from './components/Clients';
+import Header from './components/Header';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
+const client = new ApolloClient({
+	uri: 'http://localhost:3001/graphql',
+	cache: new InMemoryCache(),
+});
 function App() {
-	return <div className='App'>Hello here</div>;
+	return (
+		<>
+			<ApolloProvider client={client}>
+				<Header />
+				<Clients />
+			</ApolloProvider>
+		</>
+	);
 }
 
 export default App;
