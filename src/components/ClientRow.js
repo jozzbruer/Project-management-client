da72fbdb5ce874f4530098dcb3837e22d0031ca6
Joz-Bruer Quince <quincejozbruer@gmail.com>
2023-04-22 01:44:47 -0700

    Done and ready for deployement

--- src/components/ClientRow.js
+++ src/components/ClientRow.js
@@ -2,19 +2,21 @@ import { useMutation } from '@apollo/client';
 import { FaTrash } from 'react-icons/fa';
 import { DELETE_CLIENTS } from '../mutations/clientMutations';
 import { GET_CLIENTS } from '../queries/clientsQuery';
+import { GET_PROJECTS } from '../queries/projectQueries';
 
 const ClientRow = ({ client }) => {
 	const [deleteClient] = useMutation(DELETE_CLIENTS, {
 		variables: { id: client.id },
-		update(cache, { data: { deleteClient } }) {
-			const { clients } = cache.readQuery({ query: GET_CLIENTS });
-			cache.writeQuery({
-				query: GET_CLIENTS,
-				data: {
-					clients: clients.filter((client) => client.id !== deleteClient.id),
-				},
-			});
-		},
+		refetchQueries: [{ query: GET_CLIENTS }, { query: GET_PROJECTS }],
+		// update(cache, { data: { deleteClient } }) {
+		// 	const { clients } = cache.readQuery({ query: GET_CLIENTS });
+		// 	cache.writeQuery({
+		// 		query: GET_CLIENTS,npm start
+		// 		data: {
+		// 			clients: clients.filter((client) => client.id !== deleteClient.id),
+		// 		},
+		// 	});
+		// },
 	});
 	return (
 		<tr>
