a42688474520d3d7d206243170ba52bd9fe33d3d
Joz-Bruer Quince <quincejozbruer@gmail.com>
2023-04-23 23:19:37 -0700

    Deploy

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
 					<div className='container'>
 						<Routes>
 							<Route path='/Project-management-client' element={<Home />} />
-							<Route path='projects/:id' element={<Project />} />
+							<Route path='/projects/:id' element={<Project />} />
 							<Route path='*' element={<NotFound />} />
 						</Routes>
 					</div>
