f2d5fbd0744b72953c96b4dc832cb1970d1069be
Joz-Bruer Quince <quincejozbruer@gmail.com>
2023-04-23 23:55:31 -0700

    Deploy

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
+import { Link } from 'react-router-dom';
+
 const Header = () => {
 	return (
 		<nav className='navbar bg-light mb-4 p-0'>
 			<div className='container'>
-				<a className='navbar-brand' href='#'>
+				<Link to='/' className='navbar-brand'>
 					<div className='d-flex'>
 						<div>ProjectMgnt</div>
 					</div>
-				</a>
+				</Link>
 			</div>
 		</nav>
 	);
